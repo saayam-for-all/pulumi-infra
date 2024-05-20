aaef6ae1d9d863f69a791c29ac6a4808f0579ebf
Dharminder Singh <dharminder@Dharminders-MBP.attlocal.net>
2024-05-19 22:45:53 -0700

    Improve networking modules, with better parenting and naming

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Saayaam Infrastructure Module
+# README #
+
+## What is this repository for? ##
+
+### This repository will manage all the underlying infrastructure components that are not application-specific but are essential for running those applications. This includes: ###
+
+* VPC, Subnets, and Networking: The network infrastructure including the VPC, subnets, NAT gateways, and route tables.
+* EKS Cluster: Configuration of the Kubernetes cluster, including versions, subnets, and associated security groups.
+* IAM Roles: Necessary for EKS and node groups, along with any IAM policies for logging, monitoring, and other AWS services.
+* Node Groups: Definitions of various node groups that might have different scaling properties or instance types.
+* Shared Services: Things like RDS databases, ElasticCache clusters, or shared S3 buckets.
+* Security Configurations: Security group definitions, access roles, any firewall configurations, etc.
+
+This repository’s main goal is to set up and configure resources that provide a platform for deploying applications.
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+
--- src/main/java/com/saayaam/infrastructure/Networking.java
+++ src/main/java/com/saayaam/infrastructure/Networking.java
@@ -39,30 +39,30 @@ public class Networking extends ComponentResource {
     super("timely:infrastructure:Networking", naming.annotate(name));
     CustomResourceOptions parent = CustomResourceOptions.builder().parent(this).build();
     vpc = vpc(naming, parent, args.getVipCIDR());
-    igw = igw(naming, parent, vpc.id());
+    igw = igw(naming, vpc);
 
     // AZ 1
     publicSubnet = publicSubnet(
-        "public-subnet", naming, parent, args.getPublicCIDR(), args.getAz1(), vpc.id());
+        "public-subnet", naming, vpc, args.getPublicCIDR(), args.getAz1());
     privateSubnet = privateSubnet(
-        "private-subnet", naming, parent, args.getPrivateCIDR(), args.getAz1(), vpc.id());
+        "private-subnet", naming, vpc, args.getPrivateCIDR(), args.getAz1());
     publicRouteTable = publicRouteTable(
-        "public-route-table", naming, parent, publicSubnet, igw, vpc.id());
-    var natEip = eip("eip-for-NAT-gw", naming, parent);
-    var natGW = natGW("public-nat-gw", naming, publicSubnet, natEip, parent);
-    routeToNATFromPrivateSubnet("route-to-nat", naming, parent, vpc.id(), natGW, privateSubnet);
+        "public-route-table", naming, vpc, publicSubnet, igw);
+    var natEip = eip("eip-for-NAT-gw", naming, vpc);
+    var natGW = natGW("public-nat-gw", naming, publicSubnet, natEip, vpc);
+    routeToNATFromPrivateSubnet("route-to-nat", naming, vpc, natGW, privateSubnet);
 
 
     // AZ 2
     publicSubnet2 = publicSubnet(
-        "public-subnet-2", naming, parent, args.getPublicCIDR2(), args.getAz2(), vpc.id());
+        "public-subnet-2", naming, vpc, args.getPublicCIDR2(), args.getAz2());
     privateSubnet2 = privateSubnet(
-        "private-subnet2", naming, parent, args.getPrivateCIDR2(), args.getAz2(), vpc.id());
+        "private-subnet2", naming, vpc, args.getPrivateCIDR2(), args.getAz2());
     publicRouteTable2 = publicRouteTable(
-        "public-route-table-2", naming, parent, publicSubnet2, igw, vpc.id());
-    var natEip2 = eip("eip-for-NAT-gw-2", naming, parent);
-    var natGW2 = natGW("public-nat-gw-2", naming, publicSubnet2, natEip2, parent);
-    routeToNATFromPrivateSubnet("route-to-nat2", naming, parent, vpc.id(), natGW2, privateSubnet2);
+        "public-route-table-2", naming, vpc, publicSubnet2, igw);
+    var natEip2 = eip("eip-for-NAT-gw-2", naming, vpc);
+    var natGW2 = natGW("public-nat-gw-2", naming, publicSubnet2, natEip2, vpc);
+    routeToNATFromPrivateSubnet("route-to-nat2", naming, vpc, natGW2, privateSubnet2);
 
 
   }
@@ -83,30 +83,30 @@ public class Networking extends ComponentResource {
   }
 
   private static InternetGateway igw(
-      Naming naming, CustomResourceOptions parent, Output<String> vpcID) {
+      Naming naming, Vpc vpc) {
     String igwName = naming.annotate("igw");
     return new InternetGateway(
         igwName,
         InternetGatewayArgs.builder()
-            .vpcId(vpcID)
+            .vpcId(vpc.id())
             .build(),
-        parent);
+        CustomResourceOptions.builder().parent(vpc).build());
   }
 
   private static RouteTable publicRouteTable(
-      String name, Naming naming, CustomResourceOptions parent,
-      Subnet subnet, InternetGateway igw, Output<String> vpcID) {
+      String name, Naming naming, Vpc vpc, Subnet subnet, InternetGateway igw) {
+    CustomResourceOptions parent = CustomResourceOptions.builder().parent(vpc).build();
     String publicRouteTableName = naming.annotate(name);
     RouteTable routeTable = new RouteTable(
         publicRouteTableName,
         RouteTableArgs.builder()
-            .vpcId(vpcID)
+            .vpcId(vpc.id())
             .build(),
         parent);
 
     // Create a route in the Route Table that directs internet-bound traffic to the IGW
     var defaultRoute = new Route(
-        naming.annotate(name+ "-route-to-internet"),
+        naming.annotate(name,"route-to-internet"),
         RouteArgs.builder()
             .routeTableId(routeTable.id())
             .destinationCidrBlock("0.0.0.0/0")
@@ -115,7 +115,7 @@ public class Networking extends ComponentResource {
         parent);
 
     var publicRouteTableAssociation = new RouteTableAssociation(
-        naming.annotate(name + "-association"),
+        naming.annotate(name,"association"),
         RouteTableAssociationArgs.builder()
             .routeTableId(routeTable.id())
             .subnetId(subnet.id())
@@ -125,13 +125,12 @@ public class Networking extends ComponentResource {
   }
 
   private static Subnet publicSubnet(
-      String name, Naming naming, CustomResourceOptions parent,
-      Output<String> cidrBlock, AZ az, Output<String> vpcID) {
+      String name, Naming naming, Vpc vpc, Output<String> cidrBlock, AZ az) {
     String publicSubnetName = naming.annotate(name);
     return  new Subnet(
         publicSubnetName,
         SubnetArgs.builder()
-            .vpcId(vpcID)
+            .vpcId(vpc.id())
             .cidrBlock(cidrBlock)
             .mapPublicIpOnLaunch(true) // Makes it a public subnet
             .availabilityZone(az.value())
@@ -140,17 +139,16 @@ public class Networking extends ComponentResource {
                 "Name", publicSubnetName,
                 "Environment", naming.getStackName()))
             .build(),
-        parent);
+        CustomResourceOptions.builder().parent(vpc).build());
   }
 
   private static Subnet privateSubnet(
-      String name, Naming naming, CustomResourceOptions parent,
-      Output<String> cidrBlock, AZ az, Output<String> vpcID){
+      String name, Naming naming, Vpc vpc, Output<String> cidrBlock, AZ az){
     String privateSubnetName = naming.annotate(name);
     return new Subnet(
         privateSubnetName,
         SubnetArgs.builder()
-            .vpcId(vpcID)
+            .vpcId(vpc.id())
             .cidrBlock(cidrBlock)
             .mapPublicIpOnLaunch(false) // Makes it a private subnet
             .availabilityZone(az.value())
@@ -159,21 +157,21 @@ public class Networking extends ComponentResource {
                 "Name", privateSubnetName,
                 "Environment", naming.getStackName()))
             .build(),
-        parent);
+        CustomResourceOptions.builder().parent(vpc).build());
   }
 
-  private static Eip eip(String name, Naming naming, CustomResourceOptions parent) {
+  private static Eip eip(String name, Naming naming, Resource parent) {
     String eipNatGateway2 = naming.annotate(name);
     return new Eip(
         eipNatGateway2,
         EipArgs.builder()
             .domain("vpc")
             .build(),
-        parent);
+        CustomResourceOptions.builder().parent(parent).build());
   }
 
   private static NatGateway natGW(
-      String name, Naming naming, Subnet subnet, Eip eip, CustomResourceOptions parent) {
+      String name, Naming naming, Subnet subnet, Eip eip, Resource parent) {
     String publicNatGateway2 = naming.annotate(name);
     return new NatGateway(
         publicNatGateway2,
@@ -181,24 +179,23 @@ public class Networking extends ComponentResource {
             .subnetId(subnet.id())
             .allocationId(eip.id())
             .build(),
-        parent);
+        CustomResourceOptions.builder().parent(parent).build());
   }
 
   private static void routeToNATFromPrivateSubnet(
-      String name, Naming naming,
-      CustomResourceOptions parent, Output<String> vpcID,
-      NatGateway natGW, Subnet subnet) {
+      String name, Naming naming, Vpc vpc, NatGateway natGW, Subnet subnet) {
+    CustomResourceOptions parent = CustomResourceOptions.builder().parent(vpc).build();
     // Create a Route Table for the private subnet
     var privateRouteTable = new RouteTable(
-        naming.annotate(name + "-table"),
+        naming.annotate(name, "table"),
         RouteTableArgs.builder()
-            .vpcId(vpcID)
+            .vpcId(vpc.id())
             .build(),
         parent);
 
     // Add a route to the NAT Gateway in the private Route Table
     var natRoute = new Route(
-        naming.annotate(name + "-route-to-nat"),
+        naming.annotate(name, "route-to-nat"),
         RouteArgs.builder()
             .routeTableId(privateRouteTable.id())
             .destinationCidrBlock("0.0.0.0/0")
@@ -207,7 +204,7 @@ public class Networking extends ComponentResource {
         parent);
 
     String privateRouteTableAssociationName
-        = naming.annotate(name + "-route-table-association");
+        = naming.annotate(name,"route-table-association");
     var privateRouteTableAssociation = new RouteTableAssociation(
         privateRouteTableAssociationName,
         RouteTableAssociationArgs.builder()
@@ -216,5 +213,4 @@ public class Networking extends ComponentResource {
             .build(),
         parent);
   }
-
 }
